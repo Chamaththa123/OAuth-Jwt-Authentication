b3556b1b05507553e599a840385ae4e7fa3aeaef
Chamaththa123 <shamodchamaththa@gmail.com>
2025-08-15 19:40:32 +0530

    complete project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,42 @@
 import React from "react";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const navigate = useNavigate();
+
   const handleSuccess = async (credentialResponse) => {
     try {
       const res = await axios.post("http://localhost:5000/auth/google-login", {
-        token: credentialResponse.credential
+        token: credentialResponse.credential,
       });
-      console.log(res.data);
       localStorage.setItem("token", res.data.token);
+      localStorage.setItem("user", JSON.stringify(res.data.user));
+      navigate("/dashboard");
     } catch (err) {
       console.error(err);
     }
   };
+
   return (
-     <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-      <div style={{ marginTop: "100px", textAlign: "center" }}>
-        <GoogleLogin
-          onSuccess={handleSuccess}
-          onError={() => console.log("Login Failed")}
-        />
-      </div>
-    </GoogleOAuthProvider>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+        <div style={{ marginTop: "100px", textAlign: "center" }}>
+          <GoogleLogin
+            onSuccess={handleSuccess}
+            onError={() => console.log("Login Failed")}
+          />
+        </div>
+      </GoogleOAuthProvider>
+    </div>
   );
 }
 
